25c12f1ce80f294cf7c3614c77fd0e72fdcecb87
Fede Otálvares <fnotalvares@gmail.com>
2023-05-26 12:24:20 +0200

    Add UserCredentials Types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,20 @@
+import { type Request } from "express";
+
 export interface PathStructure {
   pingController: string;
 }
+
+export interface UserCredentials {
+  username: string;
+  password: string;
+}
+
+export interface UserData extends UserCredentials {
+  _id: string;
+}
+
+export type UserCredentialsRequest = Request<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  UserCredentials
+>;
